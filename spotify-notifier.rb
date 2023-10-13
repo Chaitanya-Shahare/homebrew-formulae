c3049b1c76d54a193255969a5719d0c44fe7aae0
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2023-10-13 16:33:50 +0530

    update sha256 and added test

--- spotify-notifier.rb
+++ spotify-notifier.rb
@@ -2,7 +2,7 @@ class SpotifyNotifier < Formula
   desc "A script that sends notifications for Spotify song changes."
   homepage "https://github.com/Chaitanya-Shahare/spotify-notifier"
   url "https://github.com/Chaitanya-Shahare/spotify-notifier/archive/v1.0.0.tar.gz"
-  sha256 "f7959bcfc6ad13de7786c8d871a129901d786f39d2076060716394e89a71773a"
+  sha256 "85347f3e0805e913daa51063bbf2581f9f434c75ee88f787600ba1d963bc2ba2"
   head "https://github.com/Chaitanya-Shahare/spotify-notifier.git"
 
   depends_on "terminal-notifier"
@@ -15,5 +15,9 @@ class SpotifyNotifier < Formula
     run [opt_bin/"spotify-notifier"]
     keep_alive true
     run_type :immediate
+  end
+
+  test do
+    system "#{bin}/spotify-notifier", "--version"
   end
 end
